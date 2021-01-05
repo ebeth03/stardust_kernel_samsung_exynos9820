e9777af550dfadfc6e195f601ab69a4737774d4d
ivanmeler <i_ivan@windowslive.com>
2021-01-05 18:02:46 +0000

    drivers: npu: remove git version checks for inline compiling

--- drivers/vision/npu/generated/npu-ver-info.c
+++ drivers/vision/npu/generated/npu-ver-info.c
@@ -1,1 +1,3 @@
-#include "npu-ver-info~"
+const char *npu_git_log_str = "0000000";
+const char *npu_git_hash_str = "0000000";
+const char *npu_build_info = "oItsMineZ@StardustKernel / Build on 2025-05-22 08:17:41-08:00";
