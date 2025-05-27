ThunderStorms21th/Galaxy-S10

sched: ems: energy: Fix energy CPU selection algorithm
- Consider CPU idleness to avoid excessive task packing, which hurts
overall performance
- Use cpu_util_wake() instead of cpu_util(), fixing miscalculation
that causes the selection code thinking that the CPU is overutilized
- Mark CPU utilization with task util accounted as least utilized
CPU

org commit: https://github.com/diepquynh/android_kernel_samsung_universal9810/commit/115285bc77b325a64faa7d5dbd17acce1cf90258

Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

Signed-off-by: ThunderStorms21th nalas <pinakastorm@gmail.com>

kernel/sched/ems/energy.c
@@ -200,30 +200,47 @@ unsigned int calculate_energy(struct task_struct *p, int target_cpu)
 static int find_min_util_cpu(const struct cpumask *mask, struct task_struct *p)
 {
 	unsigned long min_util = ULONG_MAX;
+	int best_idle_cpu = -1;
+	int best_idle_cstate = INT_MAX;
 	int min_util_cpu = -1;
+	int prev_cpu = task_cpu(p);
 	int cpu;
 
 	/* Find energy efficient cpu in each coregroup. */
 	for_each_cpu_and(cpu, mask, cpu_active_mask) {
 		unsigned long capacity_orig = capacity_orig_of(cpu);
 		unsigned long util = ml_task_attached_cpu_util(cpu, p);
+		unsigned long new_util = cpu_util_wake(cpu, p) + task_util_est(p);
 
 		/* Skip over-capacity cpu */
 		if (util >= capacity_orig)
 			continue;
+
+		if (idle_cpu(cpu)) {
+			int idle_idx = idle_get_state_idx(cpu_rq(cpu));
+
+			if (idle_idx > best_idle_cstate ||
+			   (idle_idx == best_idle_cstate &&
+				best_idle_cpu == prev_cpu))
+				continue;
+
+			best_idle_cstate = idle_idx;
+			best_idle_cpu = cpu;
+			continue;
+		}
 
 		/*
 		 * Choose min util cpu within coregroup as candidates.
 		 * Choosing a min util cpu is most likely to handle
 		 * wake-up task without increasing the frequecncy.
 		 */
-		if (util < min_util) {
-			min_util = util;
+		if (new_util < min_util) {
+			min_util = new_util;
 			min_util_cpu = cpu;
 		}
 	}
 
-	return min_util_cpu;
+	return cpu_selected(best_idle_cpu) ? best_idle_cpu : min_util_cpu;
 }
 
 struct eco_env {

ebeth03/stardust_kernel_samsung_exynos9820

kernel/sched/ems/energy.c
@@ -206,30 +206,47 @@ unsigned int calculate_energy(struct task_struct *p, int target_cpu)
 static int find_min_util_cpu(const struct cpumask *mask, struct task_struct *p)
 {
 	unsigned long min_util = ULONG_MAX;
+	int best_idle_cpu = -1;
+	int best_idle_cstate = INT_MAX;
 	int min_util_cpu = -1;
+	int prev_cpu = task_cpu(p);
 	int cpu;
 
 	/* Find energy efficient cpu in each coregroup. */
 	for_each_cpu_and(cpu, mask, cpu_active_mask) {
 		unsigned long capacity_orig = capacity_orig_of(cpu);
 		unsigned long util = ml_task_attached_cpu_util(cpu, p);
+		unsigned long new_util = ml_cpu_util_wake(cpu, p) + task_util(p);
 
 		/* Skip over-capacity cpu */
 		if (util >= capacity_orig)
 			continue;
+
+		if (idle_cpu(cpu)) {
+			int idle_idx = idle_get_state_idx(cpu_rq(cpu));
+
+			if (idle_idx > best_idle_cstate ||
+			   (idle_idx == best_idle_cstate &&
+				best_idle_cpu == prev_cpu))
+				continue;
+
+			best_idle_cstate = idle_idx;
+			best_idle_cpu = cpu;
+			continue;
+		}
 
 		/*
 		 * Choose min util cpu within coregroup as candidates.
 		 * Choosing a min util cpu is most likely to handle
 		 * wake-up task without increasing the frequecncy.
 		 */
-		if (util < min_util) {
-			min_util = util;
+		if (new_util < min_util) {
+			min_util = new_util;
 			min_util_cpu = cpu;
 		}
 	}
 
-	return min_util_cpu;
+	return cpu_selected(best_idle_cpu) ? best_idle_cpu : min_util_cpu;
 }
 
 struct eco_env {